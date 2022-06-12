b499e8aefe562df5ffd091e37e0c4251ad8c41b7
bkirolos <hey@bkirolos.com>
2022-06-12 20:56:09 +0000

    Initial TF creation.

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ module "vpc" {
 
   enable_nat_gateway = true
   single_nat_gateway = true
-  
+
   enable_dns_support  = true
   enable_dns_hostnames = true
 
@@ -54,7 +54,7 @@ resource "aws_default_security_group" "vpn-security-group" {
 resource "aws_security_group" "site-ec2-sec-grp" {
   name   = "${var.app_name}-frontend-sg-alb-${var.environment}"
   vpc_id = module.vpc.vpc_id
- 
+
   ingress {
    protocol         = "tcp"
    from_port        = 80
@@ -62,7 +62,7 @@ resource "aws_security_group" "site-ec2-sec-grp" {
    cidr_blocks      = ["0.0.0.0/0"]
    ipv6_cidr_blocks = ["::/0"]
   }
- 
+
   ingress {
    protocol         = "tcp"
    from_port        = 443
@@ -70,7 +70,7 @@ resource "aws_security_group" "site-ec2-sec-grp" {
    cidr_blocks      = ["0.0.0.0/0"]
    ipv6_cidr_blocks = ["::/0"]
   }
-  
+
   ingress {
    protocol         = "tcp"
    from_port        = 4202
@@ -78,7 +78,7 @@ resource "aws_security_group" "site-ec2-sec-grp" {
    cidr_blocks      = ["0.0.0.0/0"]
    ipv6_cidr_blocks = ["::/0"]
   }
- 
+
   egress {
    protocol         = "-1"
    from_port        = 0
@@ -95,7 +95,7 @@ resource "aws_lb" "site-alb" {
   load_balancer_type = "application"
   security_groups    = [aws_security_group.site-ec2-sec-grp.id]
   subnets            = module.vpc.public_subnets
- 
+
   enable_deletion_protection = true
 }
 
@@ -106,7 +106,7 @@ resource "aws_alb_target_group" "app-server-tg" {
   protocol    = "HTTP"
   vpc_id      = module.vpc.vpc_id
   target_type = "instance"
- 
+
   health_check {
    healthy_threshold   = "2"
    interval            = "15"
@@ -122,10 +122,10 @@ resource "aws_alb_listener" "frontend-http" {
   load_balancer_arn = aws_lb.site-alb.id
   port              = 80
   protocol          = "HTTP"
- 
+
   default_action {
   type = "redirect"
- 
+
     redirect {
       port        = 443
       protocol    = "HTTPS"
@@ -133,15 +133,15 @@ resource "aws_alb_listener" "frontend-http" {
     }
   }
 }
- 
+
 resource "aws_alb_listener" "frontend-https" {
   load_balancer_arn = aws_lb.site-alb.id
   port              = 443
   protocol          = "HTTPS"
- 
+
   ssl_policy        = "ELBSecurityPolicy-2016-08"
   certificate_arn   = var.alb_tls_cert_arn
- 
+
   default_action {
     target_group_arn = aws_alb_target_group.app-server-tg.id
     type             = "forward"
